637ed2c12c4c0703523b74c016dc91d48a643860
stwiggy <144397102+stwiggy@users.noreply.github.com>
2023-10-29 22:28:56 +0000

    defined stuff

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,12 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static final double kTurnSlowdown = 0.3;
+    public static final double kSpeedSlowdown = 0.6;
+
+    public static final double kP = 0.01;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kShootWantedRPM = 4000;
+}
